07bef872041d4c7f9b40b9bba9933a733d615d42
aessary <raressilviulazar@gmail.com>
2024-11-25 17:28:18 +0000

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Hero  from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
         <main className="relative min-h-screen w-screen overflow-x-hidden">
 
             <Hero />
-            <section className="z-0 min-h-screen bg-blue-500"></section>
+            <About/>
         </main>
             )
 
