5b6c313812326944c8681fdb7d594493b51eb047
aessary <raressilviulazar@gmail.com>
2024-11-27 09:11:13 +0000

    Story section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero  from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
             <Hero />
             <About/>
             <Features />
+            <Story />
         </main>
             )
 
